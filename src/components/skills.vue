d0285921ccb4d651ed71166c813a44190e45b0c5
anuskam <anuskam92@gmail.com>
2021-05-18 13:08:54 +0200

    idiomas añadidos

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -4,7 +4,21 @@
       <img src="../assets/media/img/meow_business.png">
       <h1>Habilidades</h1>
     </div>
-    <!-- <h1>Habilidades</h1> -->
+    <div class="lenguajes">
+      <div class="lenguajes-content" v-for="lenguaje in lenguajes" :key="lenguaje.id">
+        <img :src="lenguaje.img" :alt="lenguaje.nombre" width="50px">
+
+      </div>
+    </div>
+    
+    <div class="idiomas">
+      <div class="idiomas-content" v-for="idioma in idiomas" :key="idioma.id">
+        <img :src="idioma.img" :alt="idioma.nombre" width="50px">
+        <span class="nombreIdioma">{{ idioma.nombre }}</span>
+        -
+        <span class="nivel font-italic">{{ idioma.nivel }}</span>
+      </div>
+    </div>
     
   </div>
 
@@ -14,51 +28,81 @@
 export default {
   data(){
     return {
-      projects: [
+      idiomas: [
+        {
+          id: 1,
+          nombre: "Castellano",
+          nivel: "Nativo", 
+          img: require('../assets/media/img/lang-spain.svg')
+        },
+        {
+          id: 2,
+          nombre: "Catalán", 
+          nivel: "Nativo", 
+          img: require('../assets/media/img/lang-catalonia.svg')
+        },
+        {
+          id: 3,
+          nombre: "Inglés",
+          nivel: "B2.2", 
+          img: require('../assets/media/img/lang-united-kingdom.svg')
+        },
+        {
+          id: 4, 
+          nombre: "Francés", 
+          nivel: "B2.1", 
+          img: require('../assets/media/img/lang-france.svg')
+        },
+      ],
+      lenguajes: [
       {
         id: 1,
-        language: "HTML",
-        level: "80%",   
+        nombre: "HTML",
+        img: require('../assets/media/img/tech-html.svg'),
+        info: "80%",   
       },
       {
         id: 2,
-        language: "CSS, SASS",
-        level: "80%",
+        nombre: "CSS, SASS",
+        
+        info: "80%",
       },
       {
         id: 3,
-        language: "Javascript",
-        level: "80%", 
+        nombre: "Javascript",
+        img: require('../assets/media/img/tech-javascript.svg'),
+        info: "80%", 
       },
       {
         id: 4,
-        language: "Laravel, Postman (??)",
-        level: "80%",
+        nombre: "Laravel, Postman (??)",
+        img: require('../assets/media/img/tech-laravel.svg'),
+        info: "80%",
       },
       {
         id: 5,
-        language: "PHP",
-        level: "80%",
+        nombre: "PHP",
+        info: "80%",
       },
       {
         id: 6,
-        language: "Git, github",
-        level: "80%",
+        nombre: "Git, github",
+        info: "80%",
       },
       {
         id: 7,
-        language: "Vue, Angular",
-        level: "80%",
+        nombre: "Vue, Angular",
+        info: "80%",
       },
       {
         id: 8,
-        language: "Illustrator, Photoshop, InDesign",
-        level: "80%",
+        nombre: "Illustrator, Photoshop, InDesign",
+        info: "Con estas herramientas de Adobe sé maquetar, vectorizar y editar imagenes de manera autodidacta",
       },
       {
         id: 9,
-        language: "Adobe Premiere Pro",
-        level: "80%",
+        nombre: "Adobe Premiere Pro",
+        info: "Montaje de video y audio",
       },
       
     ]
@@ -93,7 +137,7 @@ export default {
 
 .cabecera {
   white-space: nowrap;
-  margin-top: 11vh;
+  margin-top: 12vh;
   /* align-items: center; */
   text-align: center;
 }
@@ -105,6 +149,23 @@ h1{
   color: white;
   /* font-family: 'Raleway', sans-serif; */
   font-family: 'Yellowtail', cursive;
+}
+
+.idiomas-content {
+  margin-bottom: 2%;
+}
+
+.nombreIdioma {
+  color: white;
+  margin-left: 1%;
+}
+
+.nivel{
+  color: white;
+}
+
+.idiomas {
+  color: white;
 }
 
 </style>
